3b28e18b0ba144ad161e61c7427790712a2d4c1b
Sorochina Elena <sorochinaelena2001@gmail.com>
2024-07-05 00:58:15 +0300

    update routing, app styles and provided http client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { HeaderComponent } from '@widgets/header';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'bend-task';
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { appPaths } from '@shared/configs/app-paths.config';
+
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: appPaths.ASYNC,
+  },
+  {
+    path: appPaths.ASYNC,
+    loadComponent: () =>
+      import('@pages/async/async-page.component').then(
+        m => m.AsyncPageComponent,
+      ),
+  },
+  {
+    path: appPaths.Subscription,
+    loadComponent: () =>
+      import('@pages/subscription/subscription-page.component').then(
+        m => m.SubscriptionPageComponent,
+      ),
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: appPaths.NOT_FOUND,
+  },
+  {
+    path: appPaths.NOT_FOUND,
+    loadComponent: () =>
+      import('@pages/not-found/not-found-page.component').then(
+        m => m.NotFoundPageComponent,
+      ),
+  },
+];
