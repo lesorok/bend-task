23ecea591b247712ec68d44514c7e1930725b6e0
Sorochina Elena <sorochinaelena2001@gmail.com>
2024-07-05 00:56:34 +0300

    added not found page

---
+++ src/pages/not-found/not-found-page.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found-page',
+  standalone: true,
+  templateUrl: './not-found-page.component.html',
+  styleUrls: ['./not-found-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NotFoundPageComponent {}
